c30e5670e58541f1c86ea714345395b25623b6db
Riccardo <bollach@virgilio.it>
2025-12-29 11:56:12 +0100

    add capsule id and cov params

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def load_module(module_name, args):
         
         if module_name == "mlst":
             return mod_pkg.Module(min_id=args.min_id_mlst, min_cov=args.min_cov_mlst)
+        elif module_name == "capsule":
+           return mod_pkg.Module(min_id=args.min_id_capsule, min_cov=args.min_cov_capsule)
         elif module_name == "virulence":
            return mod_pkg.Module(min_id=args.min_id_vir, min_cov=args.min_cov_vir)
         elif module_name == "resistance":
@@ -54,6 +56,8 @@ def parse_arguments(available_modules):
     thresh_group = parser.add_argument_group("Thresholds")
     thresh_group.add_argument("--min_id_mlst", type=float, default=95.0, help="Min identity for MLST")
     thresh_group.add_argument("--min_cov_mlst", type=float, default=95.0, help="Min coverage for MLST")
+    thresh_group.add_argument("--min_id_capsule", type=float, default=90.0, help="Min identity for Capsule")
+    thresh_group.add_argument("--min_cov_capsule", type=float, default=80.0, help="Min coverage for Capsule")
     thresh_group.add_argument("--min_id_vir", type=float, default=90.0, help="Min identity for Virulence")
     thresh_group.add_argument("--min_cov_vir", type=float, default=80.0, help="Min coverage for Virulence")
     thresh_group.add_argument("--min_id_res", type=float, default=90.0, help="Min identity for Resistance")
@@ -137,7 +141,7 @@ def main():
         "agr_type", #agr module
         "res_score", "Mec_RES", "Beta_lactamases", "Fluoroquinolones", "Tetracyclines", "Vancomycin", "Other_RES", #resistance module 
         "truncated_resistance_hits", "spurious_resistance_hits", #resistance module
-        "biofilm_score", "clfAB", "clf_genes", "fnbAB", "fnb_genes", "icaADBC", "ica_genes", "icaR_mutations", #biofilm module
+        "biofilm_score", "clfAB", "clf_genes", "fnbAB", "fnb_genes", "icaADBC", "ica_genes", "icaR_mutations", "biofilm_truncated_hits", #biofilm module
         "vir_pvl", "vir_tsst", "vir_et", "spurious_virulence_hits" #virulence module
     ]
 
