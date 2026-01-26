d9e3edcf5ce15e73d7e50b6fdd9791df2f88bb09
Riccardo <bollach@virgilio.it>
2026-01-26 13:53:27 +0100

    add sccmec subtype

--- modules/sccmec/sccmec.py
+++ modules/sccmec/sccmec.py
@@ -7,42 +7,35 @@ class Module:
     def __init__(self):
         self.name = "sccmec"
         self.module_dir = Path(__file__).parent
-        self.db_fasta = self.module_dir / "data" / "targets.fasta"
-        self.blast_db = self.module_dir / "data" / "sccmec_db"
+        self.targets_fasta = self.module_dir / "data" / "targets.fasta"
+        self.targets_db = self.module_dir / "data" / "sccmec_targets_db"
+        self.regions_fasta = self.module_dir /"data" / "regions.fasta"
+        self.regions_db = self.module_dir / "data" / "sccmec_regions_db"
+
 
     def check_db(self):
-        if not self.db_fasta.exists(): return False
-        if not (self.module_dir / "data" / "sccmec_db.nhr").exists():
-            cmd = ["makeblastdb", "-in", str(self.db_fasta), "-dbtype", "nucl", "-out", str(self.blast_db)]
+        if not self.targets_fasta.exists(): return False
+        if not (self.module_dir / "data" / "sccmec_targets_db.nhr").exists():
+            cmd = ["makeblastdb", "-in", str(self.targets_fasta), "-dbtype", "nucl", "-out", str(self.targets_db)]
+            subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL)
+        if not self.regions_fasta.exists(): return False
+        if not (self.module_dir / "data" / "sccmec_regions_db.nhr").exists():
+            cmd = ["makeblastdb", "-in", str(self.regions_fasta), "-dbtype", "nucl", "-out", str(self.regions_db)]
             subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL)
         return True
 
     def run(self, assembly_path):
-        cmd = [
-            "blastn", "-query", str(assembly_path), "-db", str(self.blast_db),
-            "-outfmt", "6 sseqid pident length slen",
-            "-max_target_seqs", "100"
-        ]
+        # determine type
+        target_res = self.run_blast(assembly_path, self.targets_db, min_ident=90.0, min_cov=80.0)
         
-        try:
-            res = subprocess.run(cmd, capture_output=True, text=True)
-            if not res.stdout:
-                return {"sccmec_type": "Negative", "sccmec_genes": "-"}
-
-            df = pd.read_csv(io.StringIO(res.stdout), sep="\t", names=["sseqid", "pident", "length", "slen"])
-            
-            df['cov'] = (df['length'] / df['slen']) * 100
-            
-            df = df[ (df['pident'] >= 90.0) & (df['cov'] >= 80.0) ]
-            
-            if df.empty:
-                return {"sccmec_type": "Negative", "sccmec_genes": "-"}
-            
+        sccmec_type = "Negative"
+        sccmec_genes = "-"
+        
+        if not target_res.empty:
             found_genes = set()
-            for raw_id in df['sseqid'].unique():
+            for raw_id in target_res['sseqid'].unique():
                 lower_id = raw_id.lower()
-                
-                # --- CCR Recombinases ---
+                # CCR Recombinases
                 if "ccra1" in lower_id: found_genes.add("ccrA1")
                 elif "ccrb1" in lower_id: found_genes.add("ccrB1")
                 elif "ccra2" in lower_id: found_genes.add("ccrA2")
@@ -52,38 +45,70 @@ class Module:
                 elif "ccra4" in lower_id: found_genes.add("ccrA4")
                 elif "ccrb4" in lower_id: found_genes.add("ccrB4")
                 elif "ccrb6" in lower_id: found_genes.add("ccrB6")
-                
                 # Differentiate ccrC1 vs ccrC2
                 elif "ccrc1" in lower_id: found_genes.add("ccrC1")
                 elif "ccrc2" in lower_id: found_genes.add("ccrC2")
-                elif "ccrc" in lower_id: found_genes.add("ccrC1") # Fallback if C1 not specified
-                
+                elif "ccrc" in lower_id: found_genes.add("ccrC1")
                 # Mec Complex Components
                 elif "meci" in lower_id: found_genes.add("mecI")
                 elif "mecr1" in lower_id: found_genes.add("mecR1")
                 elif "is1272" in lower_id: found_genes.add("IS1272")
                 elif "is431" in lower_id: found_genes.add("IS431")
-                
                 # Specific Insertion Sequences
-                elif "is12960d" in lower_id: found_genes.add("IS12960D") # Key for Type VII
-                
+                elif "is12960d" in lower_id: found_genes.add("IS12960D")
                 # Resistance Genes
                 elif "meca" in lower_id: found_genes.add("mecA")
                 elif "mecc" in lower_id: found_genes.add("mecC")
-                elif "blaz" in lower_id: found_genes.add("blaZ") # Helper for Type XI
+                elif "blaz" in lower_id: found_genes.add("blaZ")
 
             mec_class = self.get_mec_class(found_genes)
             ccr_complexes = self.get_ccr_complex(found_genes)
-            final_type = self.assign_type(mec_class, ccr_complexes, found_genes)
-            genes_str = ";".join(sorted(list(found_genes)))
+            sccmec_type = self.assign_type(mec_class, ccr_complexes, found_genes)
+            sccmec_genes = ";".join(sorted(list(found_genes)))
+        # determine subtype
+        sccmec_subtype = "-"
+        if self.regions_fasta.exists():
+            region_res = self.run_blast(assembly_path, self.regions_db, min_ident=90.0, min_cov=70.0)
+            
+            if not region_res.empty:
+                # Sort by bitscore/coverage to get the best hit
+                best_hit = region_res.iloc[0]['sseqid']
+                # Parse header format
+                if "|" in best_hit:
+                    sccmec_subtype = best_hit.split("|")[-1]
+                else:
+                    sccmec_subtype = best_hit 
 
-            return {
-                "sccmec_type": final_type,
-                "sccmec_genes": genes_str
-            }
+        return {
+            "sccmec_type": sccmec_type,
+            "sccmec_subtype": sccmec_subtype,
+            "sccmec_genes": sccmec_genes
+        }
 
-        except Exception as e:
-            return {"sccmec_type": "Error", "sccmec_genes": str(e)}
+    def run_blast(self, assembly_path, db_path, min_ident, min_cov):
+        """Helper to run blast and filter results"""
+        cmd = [
+            "blastn", "-query", str(assembly_path), "-db", str(db_path),
+            "-outfmt", "6 sseqid pident length slen",
+            "-max_target_seqs", "100"
+        ]
+        try:
+            res = subprocess.run(cmd, capture_output=True, text=True)
+            if not res.stdout:
+                return pd.DataFrame()
+
+            df = pd.read_csv(io.StringIO(res.stdout), sep="\t", names=["sseqid", "pident", "length", "slen"])
+            df['cov'] = (df['length'] / df['slen']) * 100
+            
+            # Filter
+            df = df[ (df['pident'] >= min_ident) & (df['cov'] >= min_cov) ]
+            
+            # Sort by coverage descending (optional but good for best hit)
+            df = df.sort_values(by='cov', ascending=False)
+            
+            return df
+        except Exception:
+            return pd.DataFrame()
 
     def get_mec_class(self, genes):
         has_mec = "mecA" in genes or "mecC" in genes
