468401c9abb03f83acaa6fb1e372e08519c864d3
Riccardo <bollach@virgilio.it>
2026-02-03 11:46:02 +0100

    prepare conda sub

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="staphscan",
+    version="0.0.1",  
+    description="A tool for Staphylococcus aureus analysis",
+    author="Riccardo Bollini",  
+    url="https://github.com/riccabolla/StaphSCAN", 
+    
+     packages=find_packages(),
+    
+    py_modules=["main"],
+    
+    package_data={
+        'modules': ['*/data/*']
+    },
+    include_package_data=True,
+    
+    install_requires=requirements,
+    
+    entry_points={
+        'console_scripts': [
+            'staphscan=main:main',  
+        ],
+    },
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
