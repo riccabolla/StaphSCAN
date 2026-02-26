a8e9c5804fe41bf570c218c28087b68a555a4efd
Riccardo <bollach@virgilio.it>
2026-02-26 14:06:28 +0100

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="staphscan",
-    version="0.1.3",  
+    version="0.2.0",  
     description="A tool for Staphylococcus aureus analysis",
     author="Riccardo Bollini",  
     url="https://github.com/riccabolla/StaphSCAN", 
